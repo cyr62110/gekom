1762644c69f60b4ea0ecbb46ba7ab5bb9f9bd0e3
Cyril Vlaminck <vlaminck.cyril@gmail.com>
2018-06-19 08:18:45 +0200

    Implemented FakeWildcardType WIP: Make sure that hashCode implementations are compatible with JRE.

--- gekom-core/src/main/kotlin/fr/cvlaminck/gekom/core/reflect/FakeParameterizedType.kt
+++ gekom-core/src/main/kotlin/fr/cvlaminck/gekom/core/reflect/FakeParameterizedType.kt
@@ -6,6 +6,11 @@ import java.lang.reflect.ParameterizedType
 import java.lang.reflect.Type
 import java.util.*
 
+/**
+ * An implementation of [ParameterizedType].
+ *
+ * @since 1.0.0
+ */
 internal class FakeParameterizedType(
         private val ownerType: Type?,
         private val rawType: Type,
@@ -25,6 +30,7 @@ internal class FakeParameterizedType(
         return TypeUtils.equals(this, other)
     }
 
+    // FIXME Rewrite to be compatible with hashCode of ParameterizedTypeImpl used by JRE.
     override fun hashCode(): Int {
         var result = ownerType?.hashCode() ?: 0
         result = 31 * result + rawType.hashCode()
--- gekom-core/src/main/kotlin/fr/cvlaminck/gekom/core/reflect/FakeWildcardType.kt
+++ gekom-core/src/main/kotlin/fr/cvlaminck/gekom/core/reflect/FakeWildcardType.kt
@@ -1,4 +1,47 @@
 package fr.cvlaminck.gekom.core.reflect
 
-internal class FakeWildcardType {
+import fr.cvlaminck.gekom.core.reflect.utils.TypeUtils
+import java.lang.reflect.Type
+import java.lang.reflect.WildcardType
+import java.util.*
+
+/**
+ * An implementation of [WildcardType].
+ *
+ * The implementation only supports wildcard type that are legitimated in JRE 6 to 11 specifications, which means:
+ * - only one upper bound and one lower bound
+ * - if a lower bound is defined, then the upper bound is Object.
+ *
+ * The implementation of [hashCode] is specially made to be compatible so you can use [FakeWildcardType] in hash-driven
+ * stuctures as well as legitimately created [WildcardType].
+ *
+ * @since 1.0.0
+ */
+class FakeWildcardType(
+        private val upperBound: Type = Object::class.java,
+        private val lowerBound: Type? = null
+) : WildcardType {
+
+    init {
+        if (lowerBound != null && upperBound == Object::class.java) {
+            throw IllegalArgumentException("As JRE 6 to 11 specifications, if an lower bound is specified, the upper bound will always be Object.")
+        }
+    }
+
+    override fun getUpperBounds(): Array<Type> = arrayOf(upperBound)
+
+    override fun getLowerBounds(): Array<Type> = if (lowerBound != null) arrayOf(lowerBound) else arrayOf()
+
+    override fun equals(other: Any?): Boolean {
+        if (other !is WildcardType) {
+            return false
+        }
+        return TypeUtils.equals(this, other)
+    }
+
+    override fun hashCode(): Int {
+        var result = 31 + upperBound.hashCode()
+        result = result xor if (lowerBound != null) 31 + lowerBound.hashCode() else 1
+        return result
+    }
 }
